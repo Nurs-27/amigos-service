72742797c1b6cf6b20a1b59591d377046cca8e9c
VTB4061215 <Madinka1127>
2022-02-26 23:55:31 +0300

    adding distributed tracing

--- notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
+++ notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
@@ -2,7 +2,13 @@ package com.amigoscode.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
+@EnableFeignClients(
+        basePackages = "com.amigoscode.client"
+)
 @SpringBootApplication
 public class NotificationApplication {
 
